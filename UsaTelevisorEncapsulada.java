6dcca29dbef6cab042b454dd4f8dbc37d1c19cc0
David Guilherme B. Denunci <118739281+davidgd11@users.noreply.github.com>
2024-03-07 09:02:07 -0300

    Aula get & set

--- UsaTelevisorEncapsulada.java
+++ UsaTelevisorEncapsulada.java
@@ -5,14 +5,41 @@ public class UsaTelevisorEncapsulada {
 		
 		TelevisorEncapsulada tv = new TelevisorEncapsulada();
 		
-		tv.setCanal(2);
-		tv.setVolume(19);
-		tv.mostrar();
+		// tv.setCanal(2);
+		// tv.setVolume(19);
+		// tv.mostrar();
 		
-		tv.aumentarVolume();
-		tv.aumentarVolume();
-		tv.setCanal(8);
-		tv.mostrar();
+		// tv.aumentarVolume();
+		// tv.aumentarVolume();
+		// tv.setCanal(8);
+		// tv.mostrar();
+
+		int escolha = 0;
+		Scanner scan;
+		try{
+			scan = new Scanner(System.in);
+			System.out.println("Escolha um canal e definida o volume");
+			tv.setCanal(scan.nextInt());
+			tv.setVolume(scan.nextInt());
+			System.out.println("Faça sua escolha: "
+					  + "\n(1) Definir canal"
+					  + "\n(2) Definir volume"
+					  + "\n(3) Aumentar volume"
+					  + "\n(4) Diminuir volume");
+			escolha = scan.nextInt();
+			if (escolha == 1){
+				System.out.println("Escolha novo canal");
+				tv.setCanal(scan.nextInt());
+			} else if (escolha == 2) {
+				System.out.println("Escolha novo volume");
+				tv.setVolume(scan.nextInt());
+			} else if (escolha == 3){
+				System.out.println("Aumentando volume...");
+				tv.aumentarVolume();
+			}
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+		}
 		
 	}
 
